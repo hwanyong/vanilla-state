282cf4d4f628b98a3640bc2123b2616e24e90d2e
UHD <yoo.hwanyong@gmail.com>
2025-02-16 11:14:22 -0800

    Refactor VnlState class for improved readability and consistency

--- dist/vanilla-state.esm.js
+++ dist/vanilla-state.esm.js
@@ -1,55 +1,55 @@
 class VnlState {
-    constructor() {
-        this._state = {};
-        this._listeners = {};
-        return new Proxy(this, {
-            get: (target, prop) => {
-                if (prop in target) return target[prop];
-                return target._state[prop];
-            },
-            set: (target, prop, value) => {
-                if (prop in target) {
-                    target[prop] = value;
-                } else {
-                    target._setState(prop, value, true);
-                }
-                return true;
-            }
-        });
-    }
-
-    addEventListener(prop, callback) {
-        if (!this._listeners[prop]) {
-            this._listeners[prop] = new Set();
+  constructor() {
+    this._state = {};
+    this._listeners = {};
+    return new Proxy(this, {
+      get: (target, prop) => {
+        if (prop in target) return target[prop];
+        return target._state[prop];
+      },
+      set: (target, prop, value) => {
+        if (prop in target) {
+          target[prop] = value;
+        } else {
+          target._setState(prop, value, true);
         }
-        this._listeners[prop].add(callback);
-    }
+        return true;
+      }
+    });
+  }
 
-    removeEventListener(prop, callback) {
-        if (this._listeners[prop]) {
-            this._listeners[prop].delete(callback);
-            if (this._listeners[prop].size === 0) {
-                delete this._listeners[prop];
-            }
-        }
+  addEventListener(prop, callback) {
+    if (!this._listeners[prop]) {
+      this._listeners[prop] = new Set();
     }
+    this._listeners[prop].add(callback);
+  }
 
-    _notify(prop) {
-        if (this._listeners[prop]) {
-            this._listeners[prop].forEach(callback => callback(this._state[prop]));
-        }
+  removeEventListener(prop, callback) {
+    if (this._listeners[prop]) {
+      this._listeners[prop].delete(callback);
+      if (this._listeners[prop].size === 0) {
+        delete this._listeners[prop];
+      }
     }
+  }
 
-    _setState(prop, value, notify = true) {
-        this._state[prop] = value;
-        if (notify) {
-            this._notify(prop);
-        }
+  _notify(prop) {
+    if (this._listeners[prop]) {
+      this._listeners[prop].forEach(callback => callback(this._state[prop]));
     }
+  }
 
-    setWithoutNotify(prop, value) {
-        this._setState(prop, value, false);
+  _setState(prop, value, notify = true) {
+    this._state[prop] = value;
+    if (notify) {
+      this._notify(prop);
     }
+  }
+
+  setWithoutNotify(prop, value) {
+    this._setState(prop, value, false);
+  }
 }
 
 export { VnlState as default };
